0eb27e3b778af6743fe6e4a36bd87f58bcb8dbbf
Nurulwafaaa <norolwafa@gmail.com>
2025-05-05 09:08:47 +0800

    Add files via upload

---
+++ doctype.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Beranda </title>
+    </head>
+<body>
+    <h1>Selamat Datang di Desa Wisata Pulesari </h1>
+    <P>Wisata Alam dan Budaya Tradisi </P>
+</body>
+</html>            
+
